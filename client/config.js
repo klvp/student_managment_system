557c6976e56e49dd16c3f0be7bdae651b54313b0
Kuna Lakshmi Vara Prasad <kunalakshmi.prasad@nw18.com>
2025-02-13 22:43:28 +0530

    update root api

--- client/config.js
+++ client/config.js
@@ -1,3 +1,4 @@
 export const config = {
-    apiBaseUrl: "http://localhost:5000/api"
+    // apiBaseUrl: "http://localhost:5000/api"
+    apiBaseUrl: "https://student-managment-system-nx3q.onrender.com/api"
 }
